790a0ed8d570b1affa7e3cc8e58a295f5f546bc6
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-04-04 11:07:12 +0530

    Pong game is done

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isPlayer1;
+    public float speed;
+    private float movement;
+    public Rigidbody2D rb;
+    public Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayer1)
+        {
+            movement = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            movement = Input.GetAxis("Vertical2");
+        }
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+
+    public void RestartGame()
+    {
+        rb.velocity = Vector3.zero;
+        transform.position = startPosition;
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int score1;
+    public int score2;
+    public Text score1Text;
+    public Text score2Text;
+    PlayerMovement player1;
+    PlayerMovement player2;
+    BallMovement ball;
+
+    private void Start()
+    {
+        player1 = GameObject.Find("Paddle1").GetComponent<PlayerMovement>();
+        player2 = GameObject.Find("Paddle2").GetComponent<PlayerMovement>();
+        ball = GameObject.Find("Ball").GetComponent<BallMovement>();
+    }
+
+    public void Score1Calculator(int scoreValue)
+    {
+        score1 = score1 + scoreValue;
+        score1Text.text = score1.ToString();
+        ResetGame();
+    }
+
+    public void Score2Calculator(int scoreValue)
+    {
+        score2 = score2 + scoreValue;
+        score2Text.text = score2.ToString();
+        ResetGame();
+    }
+
+    public void ResetGame()
+    {
+        ball.RestartPosition();
+        player1.RestartGame();
+        player2.RestartGame();
+    }
+
+
+}
